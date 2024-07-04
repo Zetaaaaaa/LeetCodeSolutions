57797790146bc1b754f5e67051e28b125a55f92b
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2024-07-04 21:52:40 +0530

    Time: 4 ms (100%), Space: 81 MB (80.35%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
@@ -13,16 +13,11 @@ class Solution {
         ListNode curr = head.next;
 
         while (curr != null) {
-            // If next node is not 0
             if (curr.next.val != 0) {
-                // Add next.val to curr.val
                 curr.val += curr.next.val;
-                // Delete next node
                 curr.next = curr.next.next;
             } else {
-                // Delete next node, i.e 0
                 curr.next = curr.next.next;
-                // Change curr ptr to 0's next node
                 curr = curr.next;
             }
         }
