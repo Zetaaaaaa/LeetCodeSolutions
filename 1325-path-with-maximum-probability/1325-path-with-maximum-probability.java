921be6b1c1698bfb6edf9ade6ec4e94327a34266
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2024-08-27 18:05:29 +0530

    Time: 25 ms (89.98%), Space: 57.5 MB (44.74%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    static class Pair{
+        int node;
+        double prob;
+        Pair(int n , double p){
+            this.node = n;
+            this.prob = p;
+        }
+    }
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
+        for(int i=0;i<edges.length;i++){
+            int[] temp = edges[i];
+            adj.get(temp[0]).add(new Pair(temp[1],succProb[i]));
+            adj.get(temp[1]).add(new Pair(temp[0],succProb[i]));
+        }
+
+        PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->Double.compare(b.prob,a.prob));
+        double[] curr_prob = new double[n];
+        curr_prob[start]=1;
+        q.offer(new Pair(start,1));
+        while(!q.isEmpty()){
+
+            Pair pair = q.poll();
+            int node = pair.node;
+            double prob = pair.prob;
+            if(node==end) return prob;
+
+            for(Pair i:adj.get(node)){
+                if(curr_prob[i.node]<i.prob*prob){
+                      curr_prob[i.node]=i.prob*prob;
+                      q.offer(new Pair(i.node , i.prob*prob));
+                }
+            }
+        }
+
+        return 0.0;
+
+        
+    }
+}
