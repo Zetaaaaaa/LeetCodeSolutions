8207051339544015c0e9b7fd65cf16eefa001d70
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2024-09-22 23:40:06 +0530

    Time: 0 ms (100%), Space: 39.8 MB (96.9%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -0,0 +1,29 @@
+class Solution {
+    private long countSteps(long curr, long n) {
+        long steps = 0, first = curr, last = curr;
+        while (first <= n) {
+            steps += Math.min(n + 1, last + 1) - first;
+            first *= 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+
+    public int findKthNumber(int n, int k) {
+        long curr = 1;
+        k--;
+        
+        while (k > 0) {
+            long steps = countSteps(curr, n);
+            if (steps <= k) {
+                curr++;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k--;
+            }
+        }
+        
+        return (int) curr;
+    }
+}
