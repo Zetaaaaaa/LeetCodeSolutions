c6160cd27bd351e80ee7780d6df0e4c4e32f42c6
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2024-11-03 18:17:27 +0530

    Time: 1 ms (15.9%), Space: 42.4 MB (5.42%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        for(int i = 0 ;i <= s.length();i++){
+            if((s.substring(i) + s.substring(0,i)).equals(goal)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
