9e6f7e2eab247faaaedb3a375db276f949b07be1
Zetaaaaaa <95016952+Zetaaaaaa@users.noreply.github.com>
2025-02-19 13:17:37 +0530

    Time: 23 ms (26.42%), Space: 45.4 MB (15.26%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,36 +1,23 @@
 class Solution {
-    int ct;
-    String sp;
     public String getHappyString(int n, int k) {
-        if(!pnt(n,k)) return "";
-        ct = 0;
-        sp = "";
-        dfs(n , "" ,0,k);
-        return sp;
+        List<String> ans = new ArrayList<>();
+        char[] ch = {'a','b','c'};
+
+        helper(ans, n , "" , ch);
+        if(k>ans.size()) return "";
+
+        return ans.get(k-1);
     }
 
-    boolean pnt(int n , int k){
-        int ans = 3;
-        int a = n-1;
-        while(a>0){
-            ans *= 2;
-            a--;
-        }
-        return (ans<k)?false:true;
-    }
-
-    public void dfs(int n ,String st ,int i,int k ){
-        if(ct>k) return;
-        if(i == n){
-            ct++;
-            if(ct == k){
-                sp = st;
-            }
+    public void helper(List<String> ans, int n, String str, char[] ch){
+        if(str.length()==n){
+            ans.add(str);
             return;
         }
-        if(i == 0 || (i>0 && st.charAt(i-1) != 'a'))dfs(n , st+"a" , i+1 , k);
-        if(i == 0 || (i>0 && st.charAt(i-1) != 'b'))dfs(n , st+"b" , i+1 , k);
-        if(i == 0 || (i>0 && st.charAt(i-1) != 'c'))dfs(n , st+"c" , i+1 , k);
-
+        for(char c: ch){
+            if(str.isEmpty() || str.charAt(str.length()-1) != c){
+                helper(ans, n, str+c, ch);
+            }
+        }
     }
 }
