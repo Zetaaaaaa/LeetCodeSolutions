ef5ba9a1fc65559d35af431b70a5fc8c16c3ce98
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-03-31 19:17:39 +0530

    Time: 1 ms (71.72%), Space: 41.7 MB (23.88%) - LeetHub

---
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(!map.isEmpty() && map.containsKey(nums[i])){
+                if(map.get(nums[i]) == 1){
+                    sum -= nums[i];
+                    map.put(nums[i], 2);
+                }
+            }
+            else{
+                map.put(nums[i], 1);
+                sum += nums[i];
+            }
+        }
+        return sum;
+
+    }
+}
