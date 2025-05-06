212968914ad68bdbd70db85c129e3ce2ac7502d8
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-05-06 09:23:35 +0530

    Time: 1 ms (98.72%), Space: 45.4 MB (44.27%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            nums[i] 
-            += (nums[nums[i]]&1023) << 10;
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
         }
-        for(int i = 0; i < nums.length; i++){
-            nums[i] >>= 10;
-        }
-        return nums;
-
+        return ans;
     }
 }
