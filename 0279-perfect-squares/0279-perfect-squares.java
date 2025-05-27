c19f4b8da3583221d68be1f11a5fd39b9e97bc96
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-05-27 12:53:41 +0530

    Time: 1 ms (99.04%), Space: 41.1 MB (97.69%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSquares(int n) {
+        if(n == 0)  return 0;
+
+        if(isroot(n))   return 1;
+
+        for(int i = 1; i <= Math.sqrt(n); i++){
+            if(isroot(n - (i * i))) return 2;
+        }
+
+        while(n % 4 == 0)
+            n /= 4;
+        
+        if(n % 8 == 7)  return 4;
+
+
+        return 3;
+
+    }
+
+    public boolean isroot(int n){
+        int sqr = (int)Math.sqrt(n);
+
+        return n - (sqr * sqr) == 0;
+    }
+}
