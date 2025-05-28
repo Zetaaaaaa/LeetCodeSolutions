a0ca31cacbb7ea41bd98f7758157f430144818de
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-05-28 13:36:31 +0530

    Time: 1 ms (43.63%), Space: 41.3 MB (14.63%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum = 0; 
+        while(num >= 10){
+            sum = 0;
+            while(num != 0){
+                int d = num % 10;
+                sum += d;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
