bda706240eb6b0c1d1c09fa2a9dca0f085b76326
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-07-06 05:56:57 +0530

    Time: 187 ms (10.88%), Space: 82.3 MB (9.82%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,28 +1,39 @@
 class FindSumPairs {
-    int[] n1, n2;
-    Map<Integer, Integer> freq;
+
+    Map<Integer, Integer> map1 = new HashMap<>();
+    Map<Integer, Integer> map2 = new HashMap<>();
+    int[] nums1;
+    int[] nums2;
 
     public FindSumPairs(int[] nums1, int[] nums2) {
-        n1 = nums1;
-        n2 = nums2;
-        freq = new HashMap<>();
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        for (int num : nums1) {
+            map1.put(num, map1.getOrDefault(num, 0) + 1);
+        }
         for (int num : nums2) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            map2.put(num, map2.getOrDefault(num, 0) + 1);
         }
     }
 
     public void add(int index, int val) {
-        int old = n2[index];
-        freq.put(old, freq.get(old) - 1);
-        n2[index] += val;
-        freq.put(n2[index], freq.getOrDefault(n2[index], 0) + 1);
+        map2.put(nums2[index], map2.get(nums2[index]) - 1);
+        nums2[index] += val;
+        map2.put(nums2[index], map2.getOrDefault(nums2[index], 0) + 1);
     }
 
     public int count(int tot) {
-        int res = 0;
-        for (int x : n1) {
-            res += freq.getOrDefault(tot - x, 0);
+        int ans = 0;
+        for (int i : map1.keySet()) {
+            ans += map2.getOrDefault(tot - i, 0) * map1.get(i);
         }
-        return res;
+        return ans;
     }
 }
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
