d7375919133509a3cda552a865975bf802b8e620
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-08-01 02:13:54 +0530

    Time: 0 ms (100%), Space: 40.7 MB (17.45%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.java
+++ 1086-divisor-game/1086-divisor-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n == 1)  return false;
-        else    return !divisorGame(n-1);
+        if(n % 2 != 0)  return false;
+        return true;
     }
 }
