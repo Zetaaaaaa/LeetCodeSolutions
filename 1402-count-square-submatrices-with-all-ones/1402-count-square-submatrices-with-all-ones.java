0b1847017fb6852c30a427acdd8bde9483031cdc
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-08-20 22:22:47 +0530

    Time: 1456 ms (5.09%), Space: 57.7 MB (6.72%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,29 +1,43 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        //Find no of rows and columns
-        int rows=matrix.length;
-        int cols=matrix[0].length;
-
-        for(int i=1;i<rows;i++)
-        {
-            for(int j=1;j<cols;j++)
-            {
-                if(matrix[i][j]==1)
-                {
-                    matrix[i][j]+=Math.min(matrix[i-1][j],Math.min(matrix[i][j-1],matrix[i-1][j-1]));
+        int ans = 0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 1){
+                    ans++;
+                    ans = ans + bfs(matrix,i,j);
                 }
             }
         }
+        return ans;
+    }
 
-        //Number of squares possible
-        int total=0;
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<cols;j++)
-            {
-                total+=matrix[i][j];
+    public int bfs(int[][] matrix,int row,int col){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] isVisited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] directions = new int[][]{{0,1},{1,0},{1,1}};
+        queue.add(new int[]{row,col});
+        isVisited[row][col] = true;
+        int ans = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int[] curr = queue.poll();
+                for(int[] direction : directions){
+                    int r = curr[0] + direction[0];
+                    int c = curr[1] + direction[1];
+                    if(r < 0 || c < 0 || r >= matrix.length || c >= matrix[0].length || matrix[r][c] == 0){
+                        return ans;
+                    }
+                    if(!isVisited[r][c]){
+                        queue.add(new int[]{r,c});
+                    }
+                }
             }
+            ans++;
         }
-        return total;
+        return ans;
     }
 }
