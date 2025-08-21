fc0c88a30a9013b315e04ef6fbab3895108c7f9b
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-08-21 11:25:45 +0530

    Time: 7 ms (65.41%), Space: 45.1 MB (94.86%) - LeetHub

---
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSubmat(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[][] width = new int[n][m];
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    // Wrong: adds 1 to 1 when j == 0, should just assign 1
+                    width[i][j] = (j == 0 ? 1 : width[i][j - 1] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int minWidth = width[i][j];
+                for (int k = i; k >= 0 && minWidth > 0; k--) {
+                    minWidth = Math.min(minWidth, width[k][j]);
+                    res += minWidth;
+                }
+            }
+        }
+
+        return res;
+    }
+}
