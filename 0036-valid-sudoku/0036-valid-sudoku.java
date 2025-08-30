96b0747112c6804a0112942b20bccd81bb54bb3e
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-08-30 12:03:27 +0530

    Time: 1 ms (99.87%), Space: 44.3 MB (89.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,55 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        
-        HashSet mySet = new HashSet();
-
-        for(int i = 0; i < 9; i++)
+class Solution 
+{
+    public boolean isValidSudoku(char[][] board) 
+    {
+        for (int row = 0; row < 9; row++) 
         {
-            for(int j = 0; j < 9; j++)
+            for (int col = 0; col < 9; col++) 
             {
-                if(board[i][j] == '.') continue;
-
-                char currChar = board[i][j];
-
-                if(!mySet.add(currChar + "row" + i) || !mySet.add(currChar + "col" + j) || !mySet.add(currChar + "box" + i/3 + '-' + j/3)) return false;
-
-
+                if (board[row][col] != '.') 
+                {
+                    if (!isSafe(board, row, col, board[row][col])) 
+                    {
+                        return false;
+                    }
+                }
             }
         }
 
         return true;
+    }
 
+    public static boolean isSafe(char[][] sudoku, int row, int col, char digit) 
+    {
+        for (int i = 0; i < 9; i++) 
+        {
+            if (i != col && sudoku[row][i] == digit)
+            {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) 
+        {
+            if (i != row && sudoku[i][col] == digit)
+            {
+                return false;
+            } 
+        }
+
+        int subgridStartRow = (row / 3) * 3;
+        int subgridStartCol = (col / 3) * 3;
+        for (int i = subgridStartRow; i < subgridStartRow + 3; i++) 
+        {
+            for (int j = subgridStartCol; j < subgridStartCol + 3; j++) 
+            {
+                if ((i != row || j != col) && sudoku[i][j] == digit)
+                {
+                    return false;
+                } 
+            }
+        }
+
+        return true;
     }
 }
