ec7d35399673e7decd8b7d73cc80a70ee68dd811
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-09-24 10:42:35 +0530

    Time: 1 ms (99.8%), Space: 41.5 MB (12.79%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,31 +1,28 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (denominator == 0)   return "";
-        StringBuilder s = new StringBuilder();
-        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-        if(numerator < 0 && denominator > 0 || numerator > 0 && denominator < 0){
-            s.append('-');
-        }
+        if(numerator == 0)  return "0";
+        StringBuilder res = new StringBuilder();
+        if((numerator < 0) ^ (denominator < 0)) res.append("-");
         long num = Math.abs((long)numerator);
         long den = Math.abs((long)denominator);
-        long n= num/den;
-        long remainder = num % den;
-        s.append(n);
-        if(remainder == 0)   return s.toString();
-        else s.append('.');
-        while(!map.containsKey(remainder)){
-            map.put(remainder, s.length());
-            n = remainder * 10 / den;
-            remainder = remainder * 10 % den;
-            if(remainder != 0 || remainder == 0 && !map.containsKey(remainder)){
-                s.append(n);
-            }
+        res.append(num/den);
+        long rem = num % den;
+        if(rem == 0)    return res.toString();
 
+        res.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while(rem != 0){
+            if(map.containsKey(rem)){
+                int idx = map.get(rem);
+                res.insert(idx, "(");
+                res.append(")");
+                break;
+            }
+            map.put(rem, res.length());
+            rem *= 10;
+            res.append(rem/den);
+            rem %= den;
         }
-        if(remainder != 0){
-            s.insert(map.get(remainder), "(");
-            s.append(')');
-        }
-        return s.toString();
+        return res.toString();
     }
 }
