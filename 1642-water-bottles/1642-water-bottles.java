955140eb8f2e067edb514d4c95c7eacfbe784091
Ankit <95016952+Zetaaaaaa@users.noreply.github.com>
2025-10-01 11:34:45 +0530

    Time: 0 ms (100%), Space: 40.5 MB (56.56%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,5 +1,5 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        return numBottles+((numBottles - 1)/(numExchange - 1));
+        return numBottles + (numBottles - 1)/(numExchange - 1);
     }
 }
